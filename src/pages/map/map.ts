33bf01d38fcb91ac1854406dda9ed1cf30f34a64
hassanshah93 <hassan.cs93@gmail.com>
2017-06-12 10:43:47 +0500

    Practice
    Practice

---
+++ src/pages/map/map.ts
@@ -0,0 +1,88 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+
+declare var google;
+
+/**
+ * Generated class for the MapPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-map',
+  templateUrl: 'map.html',
+})
+export class MapPage {
+	
+
+
+	@ViewChild('map') mapElement: ElementRef ;
+	map: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) {
+  }
+
+  ionViewDidLoad() {
+    
+    this.initMap();
+
+  }
+
+  initMap(){
+
+  	this.geolocation.getCurrentPosition().then((position) => {
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    }, (err) => {
+      console.log(err);
+    });
+
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+    let content = "<h3>My New Location!</h3><h5>by Anish youtube - Please subscribe</h5>";
+
+
+
+
+
+
+
+  }
+
+  
+
+  addMarker() { // To Add Marker
+  	console.log();
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+    let content = "<h3>My New Location!</h3><h5>by Anish youtube - Please subscribe</h5>";
+    this.addInfoWindow(marker, content);
+  }
+ 
+  addInfoWindow(marker, content) {
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+  }
+
+}
---
+++ src/pages/reddits/reddits.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {RedditService} from '../../app/services/reddit.service';
+
+@Component({
+  selector: 'reddits',
+  templateUrl: 'reddits.html'
+})
+export class RedditsPage {
+
+	  items: any;
+
+  
+  constructor(public navCtrl: NavController, private redditService:RedditService) {
+
+
+
+  }
+
+  ngOnInit(){
+
+  	this.getPosts('Sports', 20);
+  }
+
+  getPosts(category, limit){
+    this.redditService.getPosts(category, limit).subscribe(response => {
+      this.items = response.data.children;
+    });
+  }
+
+
+  
+}
